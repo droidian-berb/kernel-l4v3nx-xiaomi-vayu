0be93184963e513f7a9e9de254d1399059599cd4
berbascum <berbascum@ticv.cat>
2025-01-23 16:18:57 +0100

    (cmdline) cgroup: add cgroup_enable=cpu cgroup_enable=cpuset

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -14,7 +14,7 @@ KERNEL_BASE_VERSION = 4.14-0
 KERNEL_DEVELOPER_NAME = l4v3nx
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 console=ttyMSM0,115200n8 systemd.unified_cgroup_hierarchy=1 buildvariant=user luks.cust.dev=/dev/mmcblk0p4 droidian.lvm.noresize
+KERNEL_BOOTIMAGE_CMDLINE = androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 console=tty0 console=ttyMSM0,115200n8 systemd.unified_cgroup_hierarchy=1 buildvariant=user luks.cust.dev=/dev/mmcblk0p4 droidian.lvm.noresize cgroup_enable=cpu cgroup_enable=cpuset
 # In development, early access available
 # droidian.lvm.noresize luks.cust.dev=/dev/mmcblk0p4
 # Boot params for debug
